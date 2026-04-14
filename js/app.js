da2422dfffae53d067fb621e623a7830f5506e24
IMF777 <133876195+IMF777@users.noreply.github.com>
2026-04-14 23:44:07 +0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
    ============================================ */
 
 // -------- API Config --------
-const API_BASE_URL = "http://localhost:3000";
+const API_BASE_URL = "https://mosque-dir.onrender.com";
 
 // -------- Data Cache --------
 let cachedMosques = null;
